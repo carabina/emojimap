2db536f67e4212cab1b778e5753f31a0ae7ab311
Matias Villaverde <a.matiasvillaverde@gmail.com>
2018-01-09 11:39:57 +0100

    Initial Commit

---
+++ EmojiMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiMap
+//
+//  Created by Matias Villaverde on 09.01.18.
+//  Copyright © 2018 Matias Villaverde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
